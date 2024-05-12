921dcd0d5ecc05d6097407e47304588abbb66af2
Thomas Breland <thomas@breland.tech>
2024-05-12 14:11:51 -0400

    vite-plugin-html to set redirect URL in 404.html

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,25 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { createHtmlPlugin } from 'vite-plugin-html';
+
+// allow base URL to be set by environment, e.g., during pipeline, for deploying to GitHub Pages subdirectory
+const BASE_URL = process.env.NODE_ENV === 'production' ? process.env.GH_PAGES_BASE_URL : '/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // allow base URL to be set by environment, e.g., during pipeline, for deploying to GitHub Pages subdirectory
-  base: process.env.NODE_ENV === 'production' ? process.env.GH_PAGES_BASE_URL : '/',
+  base: BASE_URL,
 
   plugins: [
     vue(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          BASE_URL: encodeURIComponent(BASE_URL)
+        }
+      },
+      template: 'public/404.html'
+    })
   ],
   resolve: {
     alias: {
